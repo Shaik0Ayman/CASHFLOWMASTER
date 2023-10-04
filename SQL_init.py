16b9db57bff63dfb2b212068a7eddd56aca2a945
KashyapKrishnaJ <kashyapkishnaj@gmail.com>
2023-10-04 18:51:52 +0530

    SQL_init

--- SQL_init.py
+++ SQL_init.py
@@ -1,23 +1,20 @@
 import mysql.connector as m
+def init():
+    from loginpage import sqlpass
+    mysql_pass = sqlpass()
+    mydb = m.connect(
+        host = "localhost", 
+        user = "root",
+        password = mysql_pass
+    )
 
-# Function to create a database or table
-# create(database or table , Name , if table then database name)
-from loginpage import sqlpass
-mysql_pass = sqlpass()
-mydb = m.connect(
-    host = "localhost", 
-    user = "root",
-    password = mysql_pass
-)
+    c = mydb.cursor()
+    udb = "users_cashflowmaster"
+    db = "cashflowmaster"
 
-c = mydb.cursor()
-dbname = 'CASHFLOWMASTER'
+    def db(dbname):
+        c.execute("CREATE DATABASE {}".format(dbname))
+        c.execute("USE {}".format(dbname))
 
-try:
-    c.execute("CREATE DATABASE {}".format(dbname))
-except:
-    pass
-c.execute("USE {}".format(dbname))
-
-def init_tables():
-    c.execute("CREATE TABLE monthly_salary()")
+    db(udb)
+    c.execute("CREATE TABLE users(uid INT(20) PRIMARY KEY, username varchar(30), password varchar(16)")
