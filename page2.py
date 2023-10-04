f9b58a34570f4d75750d3fc6ff0b0072c26358d4
Shaik0Ayman <shaikaymanhameedbaig@gmail.com>
2023-10-04 17:17:37 +0530

    a

---
+++ page2.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import messagebox
+top = Tk()
+def secwin():
+    top.geometry("250x200")
+    top.title("CASH FLOW MASTER")
+    top.geometry("400x400")
+    
+    # Create labels and entry widgets for Monthly salary, Average expenditure, Asset name, and Asset value
+    label1 = Label(top, text="CASH FLOW MASTER")
+    label1.pack()
+
+    global monthly_salary_entry, avg_expenditure_entry, asset_name_entry, asset_value_entry
+
+    monthly_salary_label = Label(top, text="Monthly Salary:")
+    monthly_salary_label.pack()
+    monthly_salary_entry = Entry(top, width=30)
+    monthly_salary_entry.pack()
+
+    avg_expenditure_label = Label(top, text="Average Expenditure:")
+    avg_expenditure_label.pack()
+    avg_expenditure_entry = Entry(top, width=30)
+    avg_expenditure_entry.pack()
+
+    asset_name_label = Label(top, text="Asset Name:")
+    asset_name_label.pack()
+    asset_name_entry = Entry(top, width=30)
+    asset_name_entry.pack()
+
+    asset_value_label = Label(top, text="Asset Value:")
+    asset_value_label.pack()
+    asset_value_entry = Entry(top, width=30)
+    asset_value_entry.pack()
