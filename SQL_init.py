462ab96350cd7fc43eff29c07d4e5297fb16d978
KashyapKrishnaJ <kashyapkishnaj@gmail.com>
2023-11-14 14:49:14 +0530

    Integration with gui??

--- SQL_init.py
+++ SQL_init.py
@@ -1,30 +1,33 @@
 import mysql.connector as m
 
+def data_init(cursor):
+    udb = "cashflowmaster"
+    cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(udb))
+    cursor.execute("USE {}".format(udb))
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (username varchar(30) PRIMARY KEY, password varchar(16), income int(20), exp int(20), emi int(20))")
+
 def connect_cursor(sql_pass):
     mydb = m.connect(
-        host = "localhost", 
-        user = "root",
-        password = sql_pass
+        host="localhost",
+        user="root",
+        password=sql_pass,
     )
     cursor = mydb.cursor()
+    data_init(cursor)
     return cursor
-
-def data_init(cursor):
-    udb = "users_cashflowmaster"
-    cursor.execute("CREATE DATABASE {}".format(udb))
-    cursor.execute("USE {}".format(udb))
-    cursor.execute("CREATE TABLE users(username varchar(30) PRIMARY KEY, password varchar(16), income int(20), exp int(20), emi int(20)")
-
-def create_user(cursor, uid, username, user_password):
-    cursor.execute("INSERT INTO users VALUES({}, '{}', '{}', 0, 0, 0)".format(uid, username, user_password))
-
-def user_check(cursor, username, user_password):
-    if username in cursor.execute("SELECT username FROM users WHERE username = {}".format(username)).fetchone()[0] and user_password == cursor.execute("SELECT password FROM users WHERE username = {}".format(username)).fetchone()[0]:
-        return True
 
 def create_user_tables(cursor, username):
     cursor.execute("CREATE TABLE assets_{}(asset_name varchar(20), asset_price int(20))".format(username))
     cursor.execute("CREATE TABLE expenses_{}(expense_name varchar(20), expense_price int(20))".format(username))
+
+def create_user(cursor, username, user_password):
+    cursor.execute("INSERT INTO users VALUES('{}', '{}', 0, 0, 0)".format(username, user_password))
+    create_user_tables(cursor, username)
+
+def user_check(cursor, username, user_password):
+    if (username in cursor.execute("SELECT username FROM users WHERE username = {}".format(username)).fetchone()[0] and
+        user_password in cursor.execute("SELECT password FROM users WHERE username = {}".format(username)).fetchone()[0]):
+        return True
 
 def insert_asset(cursor, username, asset_name, asset_price):
     cursor.execute("INSERT INTO assets_{} VALUES('{}', {})".format(username, asset_name, asset_price))
--- loginpage.py
+++ loginpage.py
@@ -1,7 +1,6 @@
-
+from SQL_init import *
 from tkinter import *
 from tkinter import messagebox
-
 
 root = Tk()
 
@@ -12,41 +11,36 @@ def firstwin():
     label1.pack()
     label2  = Label(root, text="enter mysql password:")
     label2.pack()
-    mysqlpass = Entry(root, width=30)
-    mysqlpass.pack()
+    sqlpass = Entry(root, width=30)
+    sqlpass.pack()
+    sql_pass = sqlpass.get()
     label3  = Label(root, text="enter username:")
     label3.pack()
     u = Entry(root, width=30)
     u.pack()
+    username = u.get()
     label4  = Label(root, text="enter password:")
     label4.pack()
     p = Entry(root, width=30)
     p.pack()
-    label4  = Label(root, text="sql password:")
-    label4.pack()
-    S = Entry(root, width=30)
-    S.pack()
+    user_password = p.get()
     
+    def verify():
+        try:
+            c = connect_cursor(sql_pass)
+        except m.Error as e:
+            error_mysql = messagebox.showwarning("ERROR", e)
+            Label(root, text= error_mysql).pack()
 
-    
-
-    global passw
-    sql_pass = mysqlpass.get()
-
-    def check():
-        user = u.get()
-        passw = p.get()
-        if ():
+        if user_check(c, username, user_password):
             root.destroy()
             from page2 import secwin
             secwin()
         else:
-            error = messagebox.showwarning("inccorect password or username", "inccorect password or username")
-            Label(root, text= error).pack()
-            
-           
-    
-    button1 = Button(root, text="submit", command=check, padx=10, pady=10, fg="green" )    
+            error_auth = messagebox.showwarning("ERROR", "Incorrect username or password")
+            Label(root, text= error_auth).pack()
+
+    button1 = Button(root, text="submit", command=verify, padx=10, pady=10, fg="green" )    
     button1.pack()
 
 
