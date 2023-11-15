7b92cb3b9a3caf7051d680d323cc0520b86b6146
Shaik0Ayman <shaikaymanhameedbaig@gmail.com>
2023-11-15 14:09:30 +0530

    darkmode

--- loginpage.py
+++ loginpage.py
@@ -5,30 +5,31 @@ from tkinter import messagebox
 root = Tk()
 
 def firstwin():
-    root.geometry("250x200")
+    root.geometry("250x230")
     root.title("LOGIN PAGE")
-    label1  = Label(root, text="CASH FLOW MASTER")
+    root.configure(bg="black")
+    label1  = Label(root, text="CASH FLOW MASTER", fg = "white", bg = "black")
     label1.pack()
-    label2  = Label(root, text="enter mysql password:")
+    label2  = Label(root, text="enter mysql password:", fg = "white", bg = "black")
     label2.pack()
-    sqlpass = Entry(root, width=30)
+    sqlpass = Entry(root, width=30, fg = "white", bg = "black")
     sqlpass.pack()
     sql_pass = sqlpass.get()
-    label3  = Label(root, text="enter username:")
+    label3  = Label(root, text="enter username:", fg = "white", bg = "black")
     label3.pack()
-    u = Entry(root, width=30)
+    u = Entry(root, width=30, fg = "white", bg = "black")
     u.pack()
     username = u.get()
-    label4  = Label(root, text="enter password:")
+    label4  = Label(root, text="enter password:", fg = "white", bg = "black")
     label4.pack()
-    p = Entry(root, width=30)
+    p = Entry(root, width=30, fg = "white", bg = "black")
     p.pack()
     user_password = p.get()
     
     def verify():
         try:
-            global c
             c = connect_cursor(sql_pass)
+            
         except m.Error as e:
             error_mysql = messagebox.showwarning("ERROR", e)
             Label(root, text= error_mysql).pack()
@@ -41,8 +42,12 @@ def firstwin():
             error_auth = messagebox.showwarning("ERROR", "Incorrect username or password")
             Label(root, text= error_auth).pack()
 
-    button1 = Button(root, text="submit", command=verify, padx=10, pady=10, bg="green" )    
+    label5  = Label(root, text="", fg = "white", bg = "black")
+    label5.pack()
+    button1 = Button(root, text="submit", command=verify, padx=10, pady=10, bg="dark green" , fg = "white")    
     button1.pack()
+    label0  = Label(root, text="", fg = "white", bg = "black")
+    label0.pack()
 
 
 firstwin()
